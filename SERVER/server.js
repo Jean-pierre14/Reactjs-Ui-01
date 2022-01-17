b323a02659d2705954d8a00f755ed96009fa1a53
Jean-pierre14 <chiruzabisimwa@gmail.com>
2022-01-17 20:03:04 +0100

    mysql config int

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,7 +1,15 @@
 const exp = require("express"),
+  cors = require("cors"),
   { success, error } = require("consola"),
   app = exp(),
+  dotenv = require("dotenv"),
   PORT = process.env.PORT || 7000;
 
+dotenv.config();
+
+app.use(cors());
+
 app.listen(PORT, (e) => {
-  if (e) error({ message: `Error: ${e}`, badge: true }
+  if (e) error({ message: `Error: ${e}`, badge: true });
+  success({ message: `Server run on port ${PORT}`, badge: true });
+});
