2df072563300522956287d307508f565997cb3fe
Jean-pierre14 <chiruzabisimwa@gmail.com>
2022-01-19 16:00:48 +0100

    We get data from our API

--- src/Components/Container/Main.jsx
+++ src/Components/Container/Main.jsx
@@ -1,17 +1,38 @@
+import React, { Component } from "react";
 import axios from "axios";
 
-export default function MainContainer() {
-  axios
-    .get("http://127.0.0.1:7000/")
-    .then(function (res) {
-      console.log(res);
-    })
-    .catch((e) => console.log(e));
-  return (
-    <>
-      <div>
-        <h3>MAin</h3>
-      </div>
-    </>
-  );
+const api = axios.create({
+  baseURL: `http://127.0.0.1:7000/`,
+});
+
+class MainContainer extends Component {
+  // Pour maintenant afficher vos informations dans la partie visuel
+  /** il va faloir creer un state pour initialiser le contenu */
+  state = {
+    users: [],
+  };
+
+  // Tu dois placer ton constructor ici pas dans le render()
+  constructor() {
+    super();
+    api.get("/").then((res) => {
+      // console.log(res.data); /** To test our API data */
+      this.setState({ users: res.data });
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <h3>Hello</h3>
+        {this.state.users.map((user) => (
+          <div key={user.id_student}>
+            <h3>{user.username}</h3>
+          </div>
+        ))}
+      </>
+    );
+  }
 }
+
+export default MainContainer;
